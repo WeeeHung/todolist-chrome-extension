25490ce4c3ffd8446d16343a4f06273f822e161b
Wee Hung <cwhung1804@gmail.com>
2023-05-04 22:21:47 +0800

    all all

---
+++ README.md
@@ -0,0 +1,38 @@
+# To Do List Chrome Extension
+#### Video Demo:  https://www.youtube.com/watch?v=x_FNdBSpBXE
+#### Description: To Do List Chrome Extension made on 4/5/2023 to boost productivity. An intro video for CS50 Final Project.
+
+## Table of Contents
+
+- [Motivation](#motivation)
+- [Installation](#installation)
+- [Usage](#usage)
+- [TechStack](#TechStack)
+- [MovingForward](#MovingForward)
+
+## Motivation
+Prior to this project, I often jot down the tasks I want to complete in Google Keep or Notepad when I am on my computer. Nonetheless, I found it very troublesome to keep alternating tabs or apps to track my taskings. Hence, I thought it would be a good idea to have a chrome extension that allows me to record my to do list. An additional setback of using notepad would be to save the notes every single time. With this chrome extension, I will not need to do manual saving as the tasks are all stored in the local storage. Hence, I can carry on with my to do list without worrying about this.
+
+## Installation
+
+In summary, the installation is simple which comprises of downloading some files. There is an image folder, a json file, html file, css file and js file required for the installation. After downloading them, go to [extensions] (chrome://extensions/) and on developer mode. Unpack the folder downloaded and activate the extension.
+You may pin the extension for convenience.
+
+## Usage
+
+There are a few basic functionalities. They are task adding, task checking and task removing. The usage should be intuitive. You may be reminded that the tasks are stored in local storage. Hence, your added tasks will remain on the list even after chrome is closed, unless it is removed with the 'x' button.
+
+![image of interface](images/ss3.png)
+
+## TechStack
+
+As mentioned, the extension is mainly built around javascript, html and css. local storage is also used to store the tasks in the list. In addition to the current tech stack, there could be external database used to store the data in case the plans moving forward will take up larger memory.
+
+## MovingForward
+
+Users can customise their to do list with the given code as they wish. If you would like to contribute your idea, please contact.
+Moving forward, there could be additional features.
+First, the clear button that allows every tasks in the list to be cleared at once, instead of removing them one by one.
+Second, a tag and filter feature to sort the tasks at hand with customisable type or priority (similar to that of notion).
+Third, I could make a swiper to change the Quote Of The Day to motivate users of the to do list.
+
---
+++ main.js
@@ -0,0 +1,113 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // array of to do items
+    var todoList = JSON.parse(localStorage.getItem('todoList')) || [];
+
+    function clearList() {
+        var ul = document.getElementById("myUL");
+        ul.innerHTML = "";
+    }
+    // init the list with remove
+    function initTheList() {
+        clearList();
+        const initList = JSON.parse(localStorage.getItem('todoList'));
+
+        // Loop through the array and display each item
+        for (const todoItem of initList) {
+            const listItem = document.createElement('li');
+            
+            listItem.textContent = todoItem.value;
+
+            // Add the "checked" class to the list item if the to-do is completed
+            if (todoItem.completed) {
+                listItem.classList.toggle('checked');
+            }
+
+            // Add the list item to the DOM
+            document.getElementById("myUL").appendChild(listItem);
+            // document.querySelector('myUL').appendChild(listItem);
+            var span = document.createElement("SPAN");
+            var txt = document.createTextNode("\u00D7");
+            span.className = "close";
+            span.appendChild(txt);
+            listItem.appendChild(span);
+        }
+        removable();
+    }
+
+    initTheList();
+
+    // Click on a close button to hide the current list item
+    function removable() {
+        var close = document.getElementsByClassName("close");
+        var i;
+        for (let i = 0; i < close.length; i++) {
+            close[i].onclick = function() {
+                console.log(close.length);
+                var div = this.parentElement;
+                var indexToRemove = Array.prototype.indexOf.call(div.parentNode.children, div);
+                div.style.display = "none";
+        
+                var todoList = JSON.parse(localStorage.getItem('todoList'));
+                todoList.splice(indexToRemove, 1);
+                // Update the local storage with the modified array
+                localStorage.setItem('todoList', JSON.stringify(todoList));
+                initTheList();
+            };
+        }
+    }
+
+    var list = document.querySelector('ul');
+    list.addEventListener('click', function(ev) {
+        if (ev.target.tagName === 'LI') {
+            ev.target.classList.toggle('checked');
+            
+            var todoList = JSON.parse(localStorage.getItem('todoList'));
+            // Find the index of the item you want to update
+            var indexToUpdate = todoList.findIndex(item => item.value === ev.target.textContent.slice(0, -1));
+            // Update the completed status of the item in the array
+            todoList[indexToUpdate].completed = !todoList[indexToUpdate].completed;
+            // Update the local storage with the modified array
+            localStorage.setItem('todoList', JSON.stringify(todoList));
+        }
+    });
+
+    var addBtn = document.getElementById("addBtn");
+    var inputField = document.getElementById("myInput");
+
+    inputField.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        addBtn.click();
+    }
+    });
+
+    addBtn.addEventListener("click", function() {
+      // Call the newElement function to add a new item to the list
+      newElement();
+    });
+
+    // Create a new list item when clicking on the "Add" button
+    function newElement() {
+        //var li = document.createElement("li");
+        var inputValue = document.getElementById("myInput").value;
+        // var t = document.createTextNode(inputValue);
+        // li.appendChild(t);
+        if (inputValue === '') {
+            alert("You must write something!");
+        } else {
+            var todoList = JSON.parse(localStorage.getItem('todoList'));
+            // Create a new to-do object
+            var todoItem = { 
+                value: inputValue, 
+                completed: false 
+              };
+            // Add the new to-do to the array
+            todoList.push(todoItem);
+            // Update the local storage with the modified array
+            localStorage.setItem('todoList', JSON.stringify(todoList));
+            document.getElementById("myInput").value = "";
+            initTheList();
+        }
+    }
+});
+
